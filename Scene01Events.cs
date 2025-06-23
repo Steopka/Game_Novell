4220c5f59a7bca6702ddbd2570b55de1256b764f
Steopka <128364430+Steopka@users.noreply.github.com>
2025-06-24 01:33:09 +0300

    Ctreate audio

---
+++ Scene01Events.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene01Event : MonoBehaviour
+{
+    public GameObject fadeScreenIn;
+    public GameObject charKasumi;
+    public GameObject charHaruka;
+    public GameObject textBox;
+    private void Start()
+    {
+        StartCoroutine(EventStarter());
+    }
+    IEnumerator EventStarter()
+    {
+        yield return new WaitForSeconds(2);
+        fadeScreenIn.SetActive(false);
+        charKasumi.SetActive(true);
+        yield return new WaitForSeconds(2);
+        textBox.SetActive(true);
+        yield return new WaitForSeconds(2);
+        charHaruka.SetActive(true);
+    }
+
+}
