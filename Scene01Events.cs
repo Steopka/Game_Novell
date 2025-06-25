c63e7ec0c4114be6fd1c651b9c61cd209dce82db
Steopka <128364430+Steopka@users.noreply.github.com>
2025-06-25 22:36:18 +0300

    fix error

--- Scene01Events.cs
+++ Scene01Events.cs
@@ -8,19 +8,50 @@ public class Scene01Event : MonoBehaviour
     public GameObject charKasumi;
     public GameObject charHaruka;
     public GameObject textBox;
+
+    [SerializeField] AudioSource girlSingh;
+    [SerializeField] AudioSource girlGasp;
+    [SerializeField] string textToSpeak;
+    [SerializeField] int currentTextLength;
+    [SerializeField] int textLength;
+    [SerializeField] GameObject mainTextObject;
+    [SerializeField] GameObject nextButton;
+    [SerializeField] int eventPos = 0;
+
+    private void Update()
+    {
+        textLength = TextCreator.charCout;
+    }
+
     private void Start()
     {
         StartCoroutine(EventStarter());
     }
     IEnumerator EventStarter()
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(2); 
         fadeScreenIn.SetActive(false);
         charKasumi.SetActive(true);
         yield return new WaitForSeconds(2);
-        textBox.SetActive(true);
+        //
+        mainTextObject.SetActive(true);
+        textToSpeak = "*-Пожалуйста... Только не снова... ";
+        textBox.GetComponent<TMPro.TMP_Text>().text = textToSpeak;
+        currentTextLength = textToSpeak.Length;
+        TextCreator.runTextPrint = true;
+        girlSingh.Play();
+        yield return new WaitForSeconds(0.05f);
+        yield return new WaitForSeconds(1);
+        yield return new WaitUntil(() => textLength == currentTextLength);
+        yield return new WaitForSeconds(0.5f);
+       
+        
+        
+        
+        textBox.SetActive(true);       
         yield return new WaitForSeconds(2);
         charHaruka.SetActive(true);
+        girlGasp.Play();
     }
 
 }
